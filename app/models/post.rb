cbc1c4918ae932fb965a49a883146bd619176d88
kooo0223 <yamakou0223@gmail.com>
2021-11-28 20:21:47 +0900

    キーワード検索機能

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,13 @@ class Post < ApplicationRecord
   validates :answer_text, presence: true
   belongs_to :user
   has_many :comments
+
+  def self.search(search)
+    if search != ""
+      Post.where('question_text LIKE(?)',"%#{search}%")
+      Post.where('answer_text LIKE(?)',"%#{search}%")
+    else
+      Post.all
+    end
+  end
 end
