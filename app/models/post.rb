c9f6c5cab86cbeeb0270bbd854bb329ed314ac2a
kooo0223 <yamakou0223@gmail.com>
2021-11-30 23:08:37 +0900

    JavaScriptでの画像投稿/プレビュー機能

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,7 @@ class Post < ApplicationRecord
   has_many   :comments
   belongs_to :user
   belongs_to :subject
+  has_many_attached :images
 
   validates :subject_id,            numericality: { other_than: 1, message: "can't be blank" }
 
