fed1a9d63577d66e67c963f4fdb6519c1a4f02a9
kooo0223 <yamakou0223@gmail.com>
2021-12-07 22:45:53 +0900

    科目ごとの表示切り替え

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,15 @@ Rails.application.routes.draw do
   root to: 'posts#index'
   resources :posts do
     resources :comments, only: :create
+
     collection do
       get 'search'
     end
+
+    member do
+      get 'choice'
+    end
+
   end
   resources :users, only: :show
 end
