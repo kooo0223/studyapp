48b38c069908549850a20a1dace44d9c9e1e92f6
kooo0223 <yamakou0223@gmail.com>
2021-12-07 15:25:42 +0900

    一覧表示画面のビュー整理

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,6 @@ class UsersController < ApplicationController
   def show
     user = User.find(params[:id])
     @nickname = current_user.nickname
-    @posts = current_user.posts
+    @posts = current_user.posts.order('created_at DESC')
   end
 end
